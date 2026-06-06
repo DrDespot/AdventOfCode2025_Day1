6a7235bcc645f0d02910318b44397394fc92eedc
DrDespot <minaboerdijk@gmail.com>
2026-06-06 11:21:55 +0200

    Input gets read

--- AoC_Day1/Program.cs
+++ AoC_Day1/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+//Console.WriteLine(Environment.CurrentDirectory);
+string input_filepath = "../../../input.txt";
+string content = File.ReadAllText(input_filepath);
+
+int dial_start = 50;
+int max_dial = 99;
+int min_dial = 0;
+int amount_of_zeros;
+
+Console.WriteLine(content);
