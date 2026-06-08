2144e3c5c4af6afb799b2a8708cf7493b5908999
DrDespot <minaboerdijk@gmail.com>
2026-06-08 21:09:43 +0200

    Welp normalizing afterrwards was appernatly not the way to go

--- AoC_Day1/Program.cs
+++ AoC_Day1/Program.cs
@@ -8,9 +8,9 @@ string input_filepath = "../../../input.txt";
 string input_content = File.ReadAllText(input_filepath);
 
 int current_dialstate = 50;
-int max_dial = 99;
-int min_dial = 0;
-int amount_of_zeros;
+const int MAX_DIAL = 99;
+const int MIN_DIAL = 0;
+int amount_of_zeros = 0;
 
 
 // Proccessing information into a usable array
@@ -42,8 +42,8 @@ foreach (var line in input_content)
     }
 }
 
-
-//current_dialstate = -563;
+//Console.WriteLine("TEST");
+//current_dialstate = 99;
 //if (current_dialstate > 99)
 //{
 //    int amount_of_divisions = current_dialstate / 100;
@@ -65,18 +65,19 @@ foreach (var line in input_content)
 //    }
 
 //}
+//Console.WriteLine(current_dialstate);
 
 
-Console.WriteLine("Array");
+Console.WriteLine("Starting Dialstate is " + current_dialstate.ToString());
 foreach (var move in processed_input)
 {
-    Console.WriteLine("Current Dialstate is " + current_dialstate.ToString());
+    
     Console.WriteLine("The dial rotated " + move.ToString());
 
 
     current_dialstate += move;
     // Normalize: force into correct range
-    if (current_dialstate > 99)
+    if (current_dialstate > MAX_DIAL)
     {
         int amount_of_divisions = current_dialstate / 100;
 
@@ -84,10 +85,9 @@ foreach (var move in processed_input)
         {
             current_dialstate -= 100;
         }
-
     }
 
-    if (current_dialstate < 0)
+    if (current_dialstate < MIN_DIAL)
     {
         int amount_of_divisions = Math.Abs(current_dialstate / 100) + 1;
         for (int i = 0; i < amount_of_divisions; i++)
@@ -97,38 +97,47 @@ foreach (var move in processed_input)
 
     }
 
+    if (current_dialstate == 0)
+    {
+        amount_of_zeros++;
+        Console.WriteLine(amount_of_zeros);
+     }
 
-    //if (current_dialstate + move >= 99) //WORKS
-    //{
-    //    current_dialstate = (current_dialstate + move) / 100;
-    //    //current_dialstate = Math.Abs(max_dial - current_dialstate - move + 1);
-    //    //if (current_dialstate >= 99)
-    //    //    current_dialstate = current_dialstate % 100;
-
-    //} else if (current_dialstate + move <= 0)
-    //{
-    //    //current_dialstate = max_dial + move + current_dialstate + 1;
-    //    //if (current_dialstate <= 0)
-    //    current_dialstate = (current_dialstate + move) / 100;
-
-
-    // THIS ONE WOULD WORK BUT C# HAS SOME MODULU QUIRKS WHICH BREAK IT
-    //if (current_dialstate + move > 99)
-    //{
-    //    current_dialstate = (current_dialstate + move) % 100;
-    //}
-    //else if (current_dialstate + move < 0)
-    //{
-    //    int abs_move = Math.Abs(current_dialstate + move);
-    //    current_dialstate = abs_move % -100;
-    //}
-    //else
-    //{
-    //    //Console.WriteLine($"*    In bounds: {current_dialstate + move}");
-    //    current_dialstate += move;
-
-    //}
-
+    Console.WriteLine("Current Dialstate is " + current_dialstate.ToString());
 
 }
- 
+
+
+Console.WriteLine("AMOUNT OF ZEROS AND FINAL ANSWER IS: " + amount_of_zeros.ToString());
+
+
+//if (current_dialstate + move >= 99) //WORKS
+//{
+//    current_dialstate = (current_dialstate + move) / 100;
+//    //current_dialstate = Math.Abs(max_dial - current_dialstate - move + 1);
+//    //if (current_dialstate >= 99)
+//    //    current_dialstate = current_dialstate % 100;
+
+//} else if (current_dialstate + move <= 0)
+//{
+//    //current_dialstate = max_dial + move + current_dialstate + 1;
+//    //if (current_dialstate <= 0)
+//    current_dialstate = (current_dialstate + move) / 100;
+
+
+// THIS ONE WOULD WORK BUT C# HAS SOME MODULU QUIRKS WHICH BREAK IT
+//if (current_dialstate + move > 99)
+//{
+//    current_dialstate = (current_dialstate + move) % 100;
+//}
+//else if (current_dialstate + move < 0)
+//{
+//    int abs_move = Math.Abs(current_dialstate + move);
+//    current_dialstate = abs_move % -100;
+//}
+//else
+//{
+//    //Console.WriteLine($"*    In bounds: {current_dialstate + move}");
+//    current_dialstate += move;
+
+//}
--- AoC_Day1/obj/Debug/net8.0/AoC_Day1.AssemblyInfo.cs
+++ AoC_Day1/obj/Debug/net8.0/AoC_Day1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AoC_Day1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbb122de91893b2351ef9a6a6beec429fa681898")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9dde9f34f50c7105bc357098d45947fa3a44f6d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("AoC_Day1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AoC_Day1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
